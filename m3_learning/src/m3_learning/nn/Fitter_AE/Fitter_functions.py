fixed masking loss and wandb logging

m3_learning/src/m3_learning/nn/Fitter_AE/Fitter_functions.py
@@ -67,7 +67,7 @@ def generate_pseudovoigt_1D(embedding, dset, limits=[1,975,25,1,25,1], device='c
     return pseudovoigt.to(torch.float32)
 
 
-def generate_pseudovoigt_2D(embedding, out_shape, limits=[1, 10, 10, 10, 10, 0.5], device='cpu', return_params=False):
+def generate_pseudovoigt_2D(embedding, out_shape, limits=[1, 10, 10, 10, 10], device='cpu', return_params=False):
     '''embedding is: 
         A: Area under curve
         I_b: baseline intensity

m3_learning/src/m3_learning/nn/Regularization/Regularizers.py
@@ -85,13 +85,18 @@ class HigherOrderLoss(nn.Module):
         self.order = order
         
     def forward(self, x1, x2):
-        return torch.sum((x1-x2)**self.order)**(1/self.order)
+        return torch.sum(abs(x1-x2)**self.order)/len(x1)
         
 class LN_loss(nn.Module):
-    def __init__(self, coef, order,):
+    def __init__(self, coef, order, weighted=False):
         super(LN_loss, self).__init__()
         self.order = order
         self.coef = coef
+        self.weighted = weighted
         
     def forward(self,x):
-        return self.coef*(x**self.order)**(1/self.order)
+        if not self.weighted:
+            return torch.sum(self.coef*(x**self.order)**(1/self.order))
+        else:
+            coef = torch.linspace(0,self.coef,x.shape[1]).repeat(x.shape[0],1)
+            return torch.sum(coef*(x**self.order)**(1/self.order))